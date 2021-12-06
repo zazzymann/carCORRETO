1f4c81e21dcd67ef7cbac8622e16f1cbda3ad2fa
zazzymann <90633631+zazzymann@users.noreply.github.com>
2021-12-06 11:19:34 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# carCORRETO
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var database;
+var gameState = 0;
+var playerCount;
+var form;
+var player;
+var game;
+var car1,car2,car3,car4;
+var cars;
+var allPlayers;
+var distance = 0
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth-20,displayHeight-30);
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    if (playerCount===4){
+        game.update(1);
+    }
+    if (gameState === 1) {
+        clear();
+        game.play();
+    }
+
+}
+
